81af1443831a0f1fb385843eb2cab77b01b4e2ea
gergely.boross <gergely.boross@aimotive.com>
2018-02-14 11:45:25 +0100

    usage help text added

--- magick_compare.sh
+++ magick_compare.sh
@@ -1,21 +1,55 @@
 #!/bin/bash
 
-if [ "$#" -le 2 ]; then
-    echo "You must be provided two folder path for comparing the files and the start frame index. ($# provided)"
-    exit 1
+usage="$(basename "$0") [-a] [-b] [-s n] -- You must be provided two folder path for comparing the images and the start frame index.
+
+where:
+    -h  show this help text
+    -a  folder1
+    -b  folder2
+    -s  set the start frame"
+
+start=0
+while getopts ':a:b:h:s:' option; do
+  case "$option" in
+    h) echo "$usage"
+       exit
+       ;;
+    s) start=$OPTARG
+       ;;
+    a) folder1=$OPTARG
+       ;;
+    b) folder2=$OPTARG
+       ;;
+    :) printf "missing argument for -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+   \?) printf "illegal option: -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+if [ -z $folder1 ]; then
+  echo "-a [option] is required"
+  exit
 fi
 
-echo "compare these folders $1, $2"
-echo "start at $3"
+if [ -z $folder2 ]; then
+  echo "-b [option] is required"
+  exit
+fi
 
 index=0
-for file in $1/*.tga; do
+for file in $folder1/*.tga; do
     index=$((index+1))
-    [ $index -le $3 ] && continue;
+    [ $index -le $start ] && continue;
 
     f=${file##*/}
-    echo "diff - $f"
-    magick compare -metric RMSE "$1/$f" "$2/$f" diff.tga
+    echo " - diff - $f"
+    magick compare -metric RMSE "$folder1/$f" "$folder2/$f" diff.tga
     feh diff.tga
 done
 
---
+++ readme.md
@@ -0,0 +1,16 @@
+-----
+usage
+-----
+magick_compare.sh [-a] [-b] [-s n] -- You must be provided two folder path for comparing the images and the start frame index.
+
+where:
+    -h  show this help text
+    -a  folder1
+    -b  folder2
+    -s  set the start frame
+
+-------
+example
+-------
+./magick_compare.sh -a ./folder2/ -b ./folder1/ -s 10
+
